4ce16d35041049f3baf964fc461f6854061c2d4a
Kanwer Abdull Rahman <kanwarabdullrahman@gmail.com>
2026-07-23 02:36:08 +0500

    feat: implement LangChain models for LLMs, Chat, and Embeddings alongside a research paper summarizer tool

--- 2.ChatModels/2_anthropic_model.py
+++
@@ -1,7 +0,0 @@
-from langchain_anthropic import ChatAnthropic
-from dotenv import load_dotenv
-load_dotenv()
-
-model = ChatAnthropic(model="claude-4.5-sonnet", temperature=0.8, max_completion_tokens=10)
-response = model.invoke("Hello, how are you?")
-print(response.content)
--- 3.EmbeddedModels/1_embedding_google_query.py
+++
@@ -1,7 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
-
-embedding=GoogleGenerativeAIEmbeddings(model="gemini-embedding-001", output_dimensionality=10)
-response=embedding.embed_query("What is the capital of Pakistan?")
-print(str(response))
